f1e1696c49094c71762420223a8601bfc90bca04
Ryan Pendleton <rpendleton@verisk.com>
2021-03-18 20:38:30 -0600

    add a patched version of uftp that works on macOS

---
+++ uftp-patched.rb
@@ -0,0 +1,54 @@
+class UftpPatched < Formula
+  desc "Secure, reliable, efficient multicast file transfer program"
+  homepage "https://uftp-multicast.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/uftp-multicast/source-tar/uftp-5.0.tar.gz"
+  sha256 "562f71ea5a24b615eb491f5744bad01e9c2e58244c1d6252d5ae98d320d308e0"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/uftp[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  depends_on "openssl@1.1"
+
+  def install
+    inreplace "uftp_common.c", "#if ((!defined WINDOWS) && (defined MCAST_JOIN_GROUP))", "#if 0"
+
+    system "make", "OPENSSL=#{Formula["openssl@1.1"].opt_prefix}", "DESTDIR=#{prefix}", "install"
+    # the makefile installs into DESTDIR/usr/..., move everything up one and remove usr
+    # the project maintainer was contacted via sourceforge on 12-Feb, he responded WONTFIX on 13-Feb
+    prefix.install Dir["#{prefix}/usr/*"]
+    (prefix/"usr").unlink
+  end
+
+  plist_options manual: "uftpd"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/uftpd</string>
+          <string>-d</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/uftp_keymgt"
+  end
+end
